6d335d85dfc3762b5359ba118318b85413ce0a01
Jason Victorio <jgvictorio@gmail.com>
2020-06-27 04:37:11 +1200

    add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,12 @@ module.exports = {
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
-        use: ['file-loader'],
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
       },
     ],
   },
