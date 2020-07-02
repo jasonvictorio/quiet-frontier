8dfe990d47fc0b8d8229bea3674f0e622f0c1ceb
Jason Victorio <jgvictorio@gmail.com>
2020-07-03 00:52:55 +1200

    add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      xl: { max: '1366px' },
+      lg: { max: '1024px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
+      xs: { max: '360px' },
+    },
     extend: {
       spacing: {
         '1x1': '100%',
